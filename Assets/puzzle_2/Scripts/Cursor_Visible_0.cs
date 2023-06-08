fddbaaaf189b147f761eab7705661e5702a18678
Sabir Süleymanlı <81561786+sabir-suleyman@users.noreply.github.com>
2023-06-08 12:07:13 +0300

    puzzle_2

---
+++ Assets/puzzle_2/Scripts/Cursor_Visible_0.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+
+public class Cursor_Visible_0 : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+        //Set Cursor to not be visible
+        Cursor.visible = true;
+        // Releases the cursor
+        Cursor.lockState = CursorLockMode.None;
+        // Locks the cursor
+        //Cursor.lockState = CursorLockMode.Locked;
+        // Confines the cursor
+        //Cursor.lockState = CursorLockMode.Confined;
+    }
+}
